786ce9161017de36d405e240cb884e274d1f80e6
MarioBros-maker <shanyibo2009@126.com>
2022-08-09 08:26:22 +0800

    Add ProgressBar function.加入ProgressBar功能（虽然并没有什么卵用

--- WindowsApp/Form1.Designer.vb
+++ WindowsApp/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form 重写 Dispose，以清理组件列表。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,9 +20,10 @@ Partial Class Form1
     '注意: 以下过程是 Windows 窗体设计器所必需的
     '可以使用 Windows 窗体设计器修改它。  
     '不要使用代码编辑器修改它。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
@@ -35,7 +36,6 @@ Partial Class Form1
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'TextBox1
@@ -164,6 +164,7 @@ Partial Class Form1
         Me.Controls.Add(Me.TextBox1)
         Me.Cursor = System.Windows.Forms.Cursors.Default
         Me.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Form1"
@@ -185,5 +186,4 @@ Partial Class Form1
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents Timer1 As Timer
-    Friend WithEvents Timer2 As Timer
 End Class
--- WindowsApp/Form1.vb
+++ WindowsApp/Form1.vb
@@ -1,36 +1,30 @@
 ﻿Public Class Form1
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ProgressBar1.Value = 0
         Dim a As Integer
         Dim b As Integer
         a = TextBox1.Text
         b = TextBox2.Text
-        If a > b Then
-            Label6.Show()
-            Label6.Text = "大于"
-        End If
-        If a < b Then
-            Label6.Show()
-            Label6.Text = "小于"
-        End If
-        If a = b Then
-            Label6.Show()
-            Label6.Text = "等于"
+        Timer1.Start()
+        ProgressBar1.Value = Timer1.Interval
+        If ProgressBar1.Value = 100 Then
+            If a > b Then
+                Label6.Show()
+                Label6.Text = "大于"
+            End If
+            If a < b Then
+                Label6.Show()
+                Label6.Text = "小于"
+            End If
+            If a = b Then
+                Label6.Show()
+                Label6.Text = "等于"
+            End If
         End If
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
         Label6.Hide()
-    End Sub
-
-    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
-
     End Sub
 
     Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
